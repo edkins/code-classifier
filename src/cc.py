110993caff15d645f02b533e1ec48e69000fbfc5
Giles Edkins <edkins@gmail.com>
2021-12-10 21:23:30 -0500

    Add/remove listings

--- src/cc.py
+++ src/cc.py
@@ -1,7 +1,10 @@
 import argparse
 import os
+import sqlite3
 import subprocess
 import sys
+
+db_name = '.code-classifier/sqlite.db'
 
 def init(args):
     os.makedirs('.code-classifier', exist_ok=True)
@@ -9,6 +12,35 @@ def init(args):
 
 def db_migrate(args):
     subprocess.run(['./.local/bin/alembic', 'upgrade', 'head'])
+
+def listing_ls(args):
+    con = sqlite3.connect(db_name)
+    cur = con.cursor()
+    count = 0
+    print('Listings:')
+    for row in cur.execute("select name,url from listing order by name asc"):
+        name, url = row
+        print(f'   {name:20} {url}')
+        count += 1
+    print(f'{count} results.')
+    con.close()
+
+def listing_add(args):
+    con = sqlite3.connect(db_name)
+    cur = con.cursor()
+    cur.execute("insert into listing(name,url) values (?,?)", (args.name, args.url))
+    con.commit()
+    con.close()
+    print(f'1 listing added')
+    pass
+
+def listing_rm(args):
+    con = sqlite3.connect(db_name)
+    cur = con.cursor()
+    rowcount = cur.execute("delete from listing where name=?", (args.name,)).rowcount
+    print(f'{rowcount} listing(s) removed')
+    con.commit()
+    con.close()
 
 def main():
     parser = argparse.ArgumentParser()
@@ -21,6 +53,18 @@ def main():
     subparsers_db = parser_db.add_subparsers()
     parser_db_migrate = subparsers_db.add_parser('migrate')
     parser_db_migrate.set_defaults(func=db_migrate)
+
+    parser_listing = subparsers.add_parser('listing')
+    subparsers_listing = parser_listing.add_subparsers()
+    parser_listing_ls = subparsers_listing.add_parser('ls')
+    parser_listing_ls.set_defaults(func=listing_ls)
+    parser_listing_add = subparsers_listing.add_parser('add')
+    parser_listing_add.set_defaults(func=listing_add)
+    parser_listing_add.add_argument('--name', required=True)
+    parser_listing_add.add_argument('--url', required=True)
+    parser_listing_rm = subparsers_listing.add_parser('rm')
+    parser_listing_rm.set_defaults(func=listing_rm)
+    parser_listing_rm.add_argument('--name', required=True)
 
     args = parser.parse_args()
     args.func(args)
