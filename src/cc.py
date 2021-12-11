59854ad9c35eb9be6b4ec55f4313a15f35ab4a94
Giles Edkins <edkins@gmail.com>
2021-12-10 20:56:33 -0500

    Use argparse

--- src/cc.py
+++ src/cc.py
@@ -1,15 +1,29 @@
+import argparse
 import os
 import subprocess
 import sys
 
+def init(args):
+    os.makedirs('.code-classifier', exist_ok=True)
+    subprocess.run(['python3','-m','pip','install','-r','requirements.txt'])
+
+def db_migrate(args):
+    subprocess.run(['./.local/bin/alembic', 'upgrade', 'head'])
+
 def main():
-    if sys.argv[1:] == ['init']:
-        os.makedirs('.code-classifier', exist_ok=True)
-        subprocess.run(['python3','-m','pip','install','-r','requirements.txt'])
-    elif sys.argv[1:] == ['db','migrate']:
-        subprocess.run(['./.local/bin/alembic', 'upgrade', 'head'])
-    else:
-        raise Exception('Unrecognized subcommand')
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    parser_init = subparsers.add_parser('init')
+    parser_init.set_defaults(func=init)
+
+    parser_db = subparsers.add_parser('db')
+    subparsers_db = parser_db.add_subparsers()
+    parser_db_migrate = subparsers_db.add_parser('migrate')
+    parser_db_migrate.set_defaults(func=db_migrate)
+
+    args = parser.parse_args()
+    args.func(args)
 
 if __name__ == '__main__':
     main()
